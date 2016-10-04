f0a89cea8c6a99fc1c218137b632d36fbb789f9a
BrandonQDixon <brandonqdixon@gmail.com>
2016-10-04 19:20:01 -0400

    File reading has been partially set up.

--- src/v1/Database.java
+++ src/v1/Database.java
@@ -1,7 +1,13 @@
 import Java.IO.File;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.Exception;
 
 /**
  * This class will maintain the database, handle database encryption, and handles modifications to the database.
+ *
+ * SO FAR: this file only creates the database locally in a non-encrypted file
+ *
  * @author Brandon Dixon
  * @version 10/4/16
  **/
@@ -9,15 +15,15 @@ public class Database {
 
     private ArrayList<String> terms;
     private final String fileName = "/info.info";
+    private File textFile = new File(filename);
+    private FileWriter write = new FileWriter(textFile,false);
+    private PrintWriter printToFile = new PrintWriter(fileWrite);
 
     /**
      * This will initalize the database and load in terms if there are any to load
      */
     public Databse() {
-	File textFile = new File(filename);
-	if (!textFile.exists()) {
-	    
-	}
+	//TODO: read the file in
     }
   
 }

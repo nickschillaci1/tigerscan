561bf3f9779f626c491bbd2f108ed825b0f573cf
Zackary Flake <flakez82@students.rowan.edu>
2016-10-07 16:29:37 -0400

    Initial creation of FileScanner

---
+++ src/v1/FileScanner.java
@@ -0,0 +1,22 @@
+package v1;
+import java.util.Scanner;
+
+/**
+ * 
+ * This class handles the scanning of a given file's text.
+ * 
+ * @author Zackary Flake
+ */
+
+public class FileScanner {
+
+	public FileScanner()
+	{
+		
+	}
+	
+	{
+		
+	}
+	
+}

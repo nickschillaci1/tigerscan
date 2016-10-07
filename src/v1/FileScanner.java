f4da24ef2bec696d92d33b2fd3b5b368ca4d7ccb
BrandonQDixon <brandonqdixon@gmail.com>
2016-10-07 15:40:47 -0400

    Scanner class started

---
+++ src/v1/FileScanner.java
@@ -0,0 +1,16 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+public class FileScanner {
+
+    public static String getStringFromFile(String fileName boolean isEncrypted) {
+	
+    }
+
+}

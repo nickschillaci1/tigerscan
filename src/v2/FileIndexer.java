9da6576e31ec233281d6b8fc9484e66aeb1eb77b
Demontoast <demontoastdt@gmail.com>
2016-10-20 15:37:13 -0400

    added lucene to classpath and made indexer class

---
+++ src/v2/FileIndexer.java
@@ -0,0 +1,22 @@
+package v2;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.index.CorruptIndexException;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.Version;
+
+public class FileIndexer {
+
+	private IndexWriter writer;
+	
+	
+}

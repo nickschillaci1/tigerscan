02f5592fc9930593736e3f06ddde89c4c7403787
Demontoast <demontoastdt@gmail.com>
2016-10-25 22:32:51 -0400

    Finished indexSearcher

--- src/lucene/FileSearcher.java
+++ src/lucene/FileSearcher.java
@@ -2,15 +2,40 @@ package lucene;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Paths;
 
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.CorruptIndexException;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
 
 public class FileSearcher {
-	QueryParser qp;
-	
-	public FileSearcher(String indexDirPath) {
-		qp = new QueryParser(LuceneConstants.CONTENTS, new StandardAnalyzer());
-		
+	QueryParser queryParser;
+	IndexSearcher indexSearcher;
+	Query query;
+
+	public FileSearcher(String indexDirPath) throws IOException {
+		queryParser = new QueryParser(LuceneConstants.CONTENTS, new StandardAnalyzer());
+
+		IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths.get(indexDirPath)));
+		indexSearcher = new IndexSearcher(reader);
+	}
+
+	public TopDocs search( String searchQuery) throws IOException, ParseException {
+		query = queryParser.parse(searchQuery);
+		return indexSearcher.search(query, LuceneConstants.MAX_SEARCH);
+	}
+
+	public Document getDocument(ScoreDoc scoreDoc) throws CorruptIndexException, IOException {
+		return indexSearcher.doc(scoreDoc.doc);	
 	}
 }
--- src/lucene/LuceneTester.java
+++ src/lucene/LuceneTester.java
@@ -2,34 +2,58 @@ package lucene;
 
 import java.io.IOException;
 
+import org.apache.lucene.document.Document;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+
 /**
  * Simple test of the Indexing process, requires user to declare file path of the data
  * they want to index and file path of where the index will be stored.
  */
 public class LuceneTester {
-	
-   String indexDir = "C:\\Users\\Ryan\\Desktop\\Index";
-   String dataDir = "C:\\Users\\Ryan\\Desktop\\test directory";
-   FileIndexer indexer;
-   
-   public static void main(String[] args) {
-      LuceneTester tester;
-      try {
-         tester = new LuceneTester();
-         tester.createIndex();
-      } catch (IOException e) {
-         e.printStackTrace();
-      } 
-   }
 
-   private void createIndex() throws IOException{
-      indexer = new FileIndexer(indexDir);
-      int numIndexed;
-      long startTime = System.currentTimeMillis();	
-      numIndexed = indexer.createIndex(dataDir, new TextFileFilter());
-      long endTime = System.currentTimeMillis();
-      indexer.closeIndex();
-      System.out.println(numIndexed+" File indexed, time taken: "
-         +(endTime-startTime)+" ms");		
-   }
+	String indexDir = "C:\\Users\\Ryan\\Desktop\\Index";
+	String dataDir = "C:\\Users\\Ryan\\Desktop\\test directory";
+	FileIndexer indexer;
+	FileSearcher searcher;
+
+	public static void main(String[] args) {
+		LuceneTester tester;
+		try {
+			tester = new LuceneTester();
+			tester.createIndex();
+			tester.search("Keepo");
+			tester.search("Doge");
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void createIndex() throws IOException{
+		indexer = new FileIndexer(indexDir);
+		int numIndexed;
+		long startTime = System.currentTimeMillis();	
+		numIndexed = indexer.createIndex(dataDir, new TextFileFilter());
+		long endTime = System.currentTimeMillis();
+		indexer.closeIndex();
+		System.out.println(numIndexed+" File indexed, time taken: "
+				+(endTime-startTime)+" ms");		
+	}
+
+	private void search(String searchQuery) throws IOException, ParseException {
+		searcher = new FileSearcher(indexDir);
+		long startTime = System.currentTimeMillis();
+		TopDocs hits = searcher.search(searchQuery);
+		long endTime = System.currentTimeMillis();
+
+		System.out.println(hits.totalHits +
+				" documents found. Time :" + (endTime - startTime) +" ms");
+		for(ScoreDoc scoreDoc : hits.scoreDocs) {
+			Document doc = searcher.getDocument(scoreDoc);
+			System.out.println("File: "+ doc.get(LuceneConstants.FILE_PATH));	//should be spitting out filepath
+		}
+	}
 }

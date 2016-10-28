c8b8e027afcd69c695fe7b738048fbb949de07a0
nickschillaci1 <nickschillaci1@gmail.com>
2016-10-27 22:24:26 -0400

    small QOL changes

--- src/v1/ScannerGUI.java
+++ src/v1/ScannerGUI.java
@@ -9,7 +9,6 @@ import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.File;
-import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ArrayList;

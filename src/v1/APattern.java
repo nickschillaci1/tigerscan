9370cae517dba922853b66fbec29f46d91db3a81
BrandonQDixon <brandonqdixon@gmail.com>
2016-11-02 15:55:36 -0400

    APattern class created

---
+++ src/v1/APattern.java
@@ -0,0 +1,11 @@
+package v1;
+
+/**
+ * This class will handle analysis of the information to determine how likely it is that an email is confidential
+ * @author Brandon Dixon
+ *
+ */
+
+public class APattern {
+
+}
